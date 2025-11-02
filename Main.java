201c882e61ef3749b4ed9c019a5c014b9f8bf68a
Devanshu <dev.ballam@gmail.com>
2025-11-02 17:30:01 +0530

    Added commented demo code for AVL Tree

--- Main.java
+++ Main.java
@@ -10,5 +10,14 @@ public class Main {
         tree.insert(3);
         tree.insert(8);
         tree.insert(26);
+
+        // AVLTree tree = new AVLTree();
+        // tree.insert(12);
+        // tree.insert(3);
+        // tree.insert(9);
+        // tree.insert(4);
+        // tree.insert(6);
+        // tree.insert(2);
+        // tree.isPerfect();
     }
 }
